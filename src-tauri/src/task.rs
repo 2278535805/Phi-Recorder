3543b712b4c27cbfbfc8212c323b47cd071f9536
HLMC <hl2278535805@outlook.com>
2025-05-20 06:25:40 +0800

    opt: reduce task wait time

--- src-tauri/src/task.rs
+++ src-tauri/src/task.rs
@@ -273,7 +273,7 @@ impl TaskQueue {
         let task = tokio::spawn(async move {
             loop {
                 let Ok(task) = receiver.try_recv() else {
-                    std::thread::sleep(std::time::Duration::from_millis(300));
+                    std::thread::sleep(std::time::Duration::from_millis(50));
                     continue;
                 };
                 if let Err(err) = task.run().await {
