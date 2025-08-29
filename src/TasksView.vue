a8d058611812efe6ac9dddca6e97f6a1358e3f41
HLMC <hl2278535805@outlook.com>
2025-08-29 22:24:08 +0800

    fix: open chart path

--- src/TasksView.vue
+++ src/TasksView.vue
@@ -186,7 +186,7 @@ function removeTask(index: number) {
           </div>
           <div class="d-flex flex-column w-100 name-cover">
             <v-card-title class="select">{{ task.name }}</v-card-title>
-            <v-card-subtitle class="mt-n2 select" style="cursor: pointer;" @click="showInFolder(task.output)">{{ task.path }}</v-card-subtitle>
+            <v-card-subtitle class="mt-n2 select" style="cursor: pointer;" @click="showInFolder(task.path)">{{ task.path }}</v-card-subtitle>
             <div class="w-100 pa-4 pb-2 pr-2 mt-2">
               <p class="mb-2 text-medium-emphasis">{{ describeStatus(task.status) }}</p>
               <template v-if="['loading', 'mixing', 'rendering'].includes(task.status.type)">
